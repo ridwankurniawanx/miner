cf98e157e999f63075d069be0fcf95dafa821d97
ridwankurniawanx <65314308+ridwankurniawanx@users.noreply.github.com>
2021-07-04 10:23:56 +0700

    Add files via upload

---
+++ c.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cd 1.29;./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user USDT:TQvtskZUJmGQzZqLFcF1pW39TYMABZZaws.bius2 --ethstratum ETHPROXY
---
+++ ctimeout.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+timeout 120s ./c.sh
+timeout 120s cd
+timeout 120s ./c.sh
+timeout 120s cd
+timeout 120s ./c.sh
+timeout 120s cd
+timeout 120s ./c.sh
+timeout 120s cd
